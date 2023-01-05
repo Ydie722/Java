9f5c8f8fe40826c181201df1eee83e25f5e76ac2
Ydie <ydie722@wp.pl>
2023-01-05 17:56:13 +0100

    api dog

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@
 // // =================================
 // // =================================
 
+const btn = document.querySelector("button");
+const img = document.querySelector("img");
 
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+btn.addEventListener("click", () => {
+	fetch(URL)
+		.then((ress) => ress.json())
+		.then((data) => img.setAttribute("src", data.message))
+		.catch((err) => console.error(err));
+});
 
 
 
